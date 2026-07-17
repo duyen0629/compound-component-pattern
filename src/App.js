3c1bd931c59b2f44e52075d704023bb58108e2fe
Chloe Pham <chloe.ph2906@gmail.com>
2026-07-17 18:55:20 +1000

    Apply compound component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
-        iconIncrease="+"
-        iconDecrease="-"
-        label="My NOT so flexible counter"
-        hideLabel={false}
-        hideIncrease={false}
-        hideDecrease={false}
-      />
+
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Increase icon="+" />
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+      </Counter>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,46 @@
-function Counter() {
-  return <span>Counter</span>;
+import { useState } from "react";
+import { createContext, useContext } from "react";
+
+// 1. Create a context
+const CounterContext = createContext();
+
+// 2. Create a parent component
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  return <CounterContext.Provider value={{ count, increase, decrease }}>{children}</CounterContext.Provider>;
 }
+
+// 3. Create a child component implement the tasks
+function Count() {
+  const { count } = useContext(CounterContext);
+  return <span>{count}</span>;
+}
+
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+
+function Increase({ icon }) {
+  const { increase } = useContext(CounterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+
+function Decrease({ icon }) {
+  const { decrease } = useContext(CounterContext);
+  return <button onClick={decrease}>{icon}</button>;
+}
+
+// 4. Add child components to the parent component
+Counter.Count = Count;
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
+
 export default Counter;
